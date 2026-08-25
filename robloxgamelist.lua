8fd21fb5ad4042553d1cd610f618d36fa7ed63a4
ramreading <ramreading77@gmail.com>
2026-08-25 13:09:35 +0800

    Update robloxgamelist.lua

--- robloxgamelist.lua
+++ robloxgamelist.lua
@@ -1,8 +1,7 @@
 local HubName = "read1ng.Hub"
 
 local SupportedGames = {
-    [139252529145498] = "https://raw.githubusercontent.com/ramreading/rescripts/refs/heads/main/blooddebt.lua",
-    [87541610558346] = "https://raw.githubusercontent.com/ramreading/rescripts/refs/heads/main/blooddebt.lua"
+    [139252529145498] = "https://raw.githubusercontent.com/ramreading/rescripts/refs/heads/main/blooddebt.lua"
 }
 
 local gameObject = game
